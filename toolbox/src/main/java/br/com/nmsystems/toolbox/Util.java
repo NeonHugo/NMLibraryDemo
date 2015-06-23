f15cf4b41293b005c987b675317bb11d50f37b0e
Neo NHugo <neonhugo@gmail.com>
2015-06-23 09:59:50 -0300

    Initial commit

---
+++ toolbox/src/main/java/br/com/nmsystems/toolbox/Util.java
@@ -0,0 +1,12 @@
+package br.com.nmsystems.toolbox;
+
+/**
+ * Created by neomatrix on 6/23/15.
+ */
+public class Util {
+
+    public static String mostrarValor(String parametro){
+        return parametro;
+    }
+
+}
